1a2bd713f5bd73f8aacbdc3f2b32eabccd97908c
stevenpslade <stevenpslade@gmail.com>
2016-07-01 11:23:58 -0700

    adding tests for user to sign up

--- spec/features/user_logs_in_feature_spec.rb
+++ spec/features/user_logs_in_feature_spec.rb
@@ -1,6 +1,32 @@
 require 'rails_helper'
 
 feature 'User signs up' do
-  scenario 'From the homepage' do
+  context 'From the homepage' do
+
+    it 'is on the hompage' do
+      visit('/')
+    end
+
+    it 'clicks sign up and goes to sign up page' do
+      click_on('Sign Up')
+      expect(page).to have_current_path('user_new_path')
+    end
+
   end
+
+  context 'Is on the sign up page' do
+
+    it 'fills/completes sign up form' do
+      fill_in('user_email', with: 'stevenpslade@gmail.com')
+      fill_in('user_password', with: 'password')
+      fill_in('user_password_confirmation', with: 'password')
+
+      click_on('Submit')
+
+      expect(page).to have_current_path('/')
+      expect(page).to have_content('You have successfully signed up.')
+    end
+
+  end
+
 end
