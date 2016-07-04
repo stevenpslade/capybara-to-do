6214a126b5765db4616da1c0f02c2af4396babe7
stevenpslade <stevenpslade@gmail.com>
2016-07-03 23:09:00 -0700

    adding tests for user log out

--- spec/features/user_logs_in_and_out_feature_spec.rb
+++ spec/features/user_logs_in_and_out_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'User logs in' do
+feature 'User logs in and out' do
 
   def log_in_with(email, password)
     visit(user_session_path)
@@ -11,23 +11,42 @@ feature 'User logs in' do
     click_on('Log in')
   end
 
-  context 'from the home page' do
+  describe 'log in process' do
+    context 'from the home page' do
 
-    it 'should click log in' do
-      visit('/')
-      click_on('Log in')
-      expect(page).to have_current_path(user_session_path)
+      it 'should click log in' do
+        visit('/')
+        click_on('Log in')
+        expect(page).to have_current_path(user_session_path)
+      end
+
+      it 'should complete log in fields' do
+        user = User.create(:email    => "stevenpslade@gmail.com", :password => "password")
+
+        log_in_with('stevenpslade@gmail.com', 'password')
+
+        expect(page).to have_current_path('/')
+        expect(page).to have_content('Signed in successfully.')
+      end
+
     end
+  end
 
-    it 'should complete log in fields' do
-      user = User.create(:email    => "stevenpslade@gmail.com", :password => "password")
+  describe 'log out process' do
+    context 'User is logged in on the home page' do
 
-      log_in_with('stevenpslade@gmail.com', 'password')
+      before :all do
+        user = User.create(:email    => "stevenpslade@gmail.com", :password => "password")
+        log_in_with('stevenpslade@gmail.com', 'password')
+      end
 
-      expect(page).to have_current_path('/')
-      expect(page).to have_content('Signed in successfully.')
+      it 'should log out' do
+        expect(page).to have_content('Log out')
+        click_on('Log out')
+        expect(page).to have_content('Signed out successfully')
+      end
+
     end
-
   end
   
 end
