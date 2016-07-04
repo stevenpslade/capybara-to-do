5c62682c7f67a13082409fa85b25fc4a2e33694e
stevenpslade <stevenpslade@gmail.com>
2016-07-03 22:50:42 -0700

    adding log in test method to keep things DRY

--- spec/features/user_logs_in_and_out_feature_spec.rb
+++ spec/features/user_logs_in_and_out_feature_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 feature 'User logs in' do
 
+  def log_in_with(email, password)
+    visit(user_session_path)
+    within('#new_user') do
+        fill_in('Email', with: email)
+        fill_in('Password', with: password)
+    end
+    click_on('Log in')
+  end
+
   context 'from the home page' do
 
     it 'should click log in' do
@@ -13,14 +22,8 @@ feature 'User logs in' do
     it 'should complete log in fields' do
       user = User.create(:email    => "stevenpslade@gmail.com", :password => "password")
 
-      visit(user_session_path)
+      log_in_with('stevenpslade@gmail.com', 'password')
 
-      within('#new_user') do
-        fill_in('Email', with: 'stevenpslade@gmail.com')
-        fill_in('Password', with: 'password')
-      end
-
-      click_on('Log in')
       expect(page).to have_current_path('/')
       expect(page).to have_content('Signed in successfully.')
     end
