8b70e30e6a1acafb6348490a903479013f0b098d
stevenpslade <stevenpslade@gmail.com>
2016-07-11 15:17:29 -0700

    adding tests for user creates new list feature

--- spec/features/user_creates_list_feature_spec.rb
+++ spec/features/user_creates_list_feature_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 feature 'User creates a list'  do
   
+  context 'from the home page' do
+
+    before :all do
+      user = User.create(:email    => "stevenpslade@gmail.com", :password => "password")
+      log_in_with('stevenpslade@gmail.com', 'password')
+    end
+
+    it 'clicks to add a new list' do
+      click_on('Add List')
+      expect(page).to have_current_path(new_list_path)
+    end
+
+    it 'fills out list info and saves it' do
+      visit(new_list_path)
+      fill_in('list_name', with: 'Another To Do List')
+      click_on('Save')
+      expect(page).to have_current_path(show_list_path)
+    end
+
+  end
+
 end
