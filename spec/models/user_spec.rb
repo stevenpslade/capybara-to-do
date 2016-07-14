085243ff981bf8202728d51100dfe372a3128502
stevenpslade <stevenpslade@gmail.com>
2016-07-14 15:14:45 -0700

    adding tests for AR associations on Users and Lists

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "active record associations between user and list" do
+
+    context "a list is created" do
+
+      before :all do
+        @user = User.create(id: 4, email: "steven@example.com", password: "password")
+        @list = @user.lists.create(title: "First List")
+      end
+
+      it "should belong to a user" do
+        # expect foreign key on @list to be user's id (4)
+      end
+
+    end
+
+    context "a user creates lists" do
+
+      before :all do
+        @user = User.create(id: 4, email: "steven@example.com", password: "password")
+        @list_one = @user.lists.create(title: "First List")
+        @list_two = @user.lists.create(title: "Another List")
+      end
+
+      it "should have many lists" do
+        # expect @user.lists to have 2 lists
+      end
+
+    end
+
+  end
+
 end
