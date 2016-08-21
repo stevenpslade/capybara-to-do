1798072ecfce0f12e2ed528e89e77bbbe56c1b0f
stevenpslade <stevenpslade@gmail.com>
2016-08-21 15:43:40 -0700

    adding capybara test for a user managing a list

--- spec/features/user_manages_lists_spec.rb
+++ spec/features/user_manages_lists_spec.rb
@@ -11,12 +11,23 @@ feature 'User manages lists' do
     before :each do
       @user = User.create(:email => 'stevenpslade@gmail.com', :password => 'password')
       log_in_with('stevenpslade@gmail.com', 'password')
+
+      visit(list_path(@list))
     end
 
     it 'edits a list' do
+      click_on('Edit List')
+      expect(page).to have_current_path(edit_list(@list))
+
+      fill_in('list_title', with: 'Edited List Title')
+      click_on('Save')
+
+      expect(page).to have_content('Edited List Title')
     end
 
     it 'deletes a list' do
+      click_on('Delete List')
+      expect(page).to have_content('List successfully deleted.')
     end
 
   end
